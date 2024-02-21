39d1007912893040e3745c066b4b48ed2085b948
kousik25 <kousikdittakavi1@gmail.com>
2024-02-21 16:13:32 +0530

    terraform

--- examples/complete/provider.tf
+++ examples/complete/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "daws76s-dev"
+    bucket = "daws76s-remote-state"
     key    = "vpc-test"
     region = "us-east-1"
     dynamodb_table = "daws76s-locking"
